e039d57e9a73830b16bb8235f105d0963b187cd2
Anas Bakhsh <bakhsh_anas@yahoo.com>
2022-07-29 00:26:41 +0300

    update tag comparison

--- Assets/Scripts/GoalTrigger.cs
+++ Assets/Scripts/GoalTrigger.cs
@@ -6,13 +6,22 @@ using UnityEngine;
 
 public class GoalTrigger : MonoBehaviour
 {
+    private GameObject goalCamera;
+    private void Start()
+    {
+        goalCamera = GameObject.Find("Goal Camera");
+        goalCamera.SetActive(false);
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name.Equals("Goal"))
+        if (other.gameObject.tag == "Finish")
         {
             print("Goal Reached!");
 
             // TODO showing score and win prompt
+            GameObject.Find("Main Camera (1stP)").SetActive(false);
+            goalCamera.SetActive(true);
+
         }
     }
 }
